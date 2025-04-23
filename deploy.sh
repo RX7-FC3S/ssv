936c45627db07fcca63bcf66b1efb3ddc5f02b14
rx7-fc3s <1269804557@qq.com>
2025-04-23 09:49:20 +0800

    :sparkles: Add cat command to display modified config.json

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ apt install shadowsocks-libev jq
 
 echo 2. Edit the server config
 jq '.server = "0.0.0.0"' $ss_config_path/config.json > ./temp_config.json && mv ./temp_config.json $ss_config_path/config.json
+cat /etc/shadowsocks-libev/config.json
 
 echo 3. Make v2ray-plugin executable
 chmod +x ./v2ray-plugin
