bb51f413f3ef12912757ba4852c1e8c6092918cf
rx7-fc3s <1269804557@qq.com>
2025-04-23 09:57:54 +0800

    ✨ Add awk command to intend apt-get install output by "apt-get: "

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ss_config_path="/etc/shadowsocks-libev"
 
 echo 1. Install shadowsocks-libev, jq
-apt-get install shadowsocks-libev jq
+apt-get install shadowsocks-libev jq | awk '{print "apt-get: " $0}'
 
 echo 2. Edit the server config
 jq '.server = "0.0.0.0"' $ss_config_path/config.json > ./temp_config.json && mv ./temp_config.json $ss_config_path/config.json
