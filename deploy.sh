b4f08d4a5510822235312b64200838b7ec4a5a6d
rx7-fc3s <1269804557@qq.com>
2025-04-30 11:28:02 +0800

    :bug: Fix tip message color bug

--- deploy.sh
+++ deploy.sh
@@ -8,34 +8,34 @@ NORMAL="\e[0m"
 ss_config_path="/etc/shadowsocks-libev"
 
 
-echo "$GREEN 1. Install shadowsocks-libev, jq $NORMAL"
+echo -e "$GREEN 1. Install shadowsocks-libev, jq $NORMAL"
 apt-get install shadowsocks-libev jq
 
-echo "$GREEN 2. Edit the server config $NORMAL"
+echo -e "$GREEN 2. Edit the server config $NORMAL"
 jq '.server = "0.0.0.0"' $ss_config_path/config.json > ./temp_config.json && mv ./temp_config.json $ss_config_path/config.json
 cat /etc/shadowsocks-libev/config.json
 
-echo "$GREEN 3. Make v2ray-plugin executable $NORMAL"
+echo -e "$GREEN 3. Make v2ray-plugin executable $NORMAL"
 chmod +x ./v2ray-plugin
 
-echo "$GREEN 4. Install acme.sh $NORMAL"
+echo -e "$GREEN 4. Install acme.sh $NORMAL"
 curl https://get.acme.sh | sh
 export PATH=$PATH:$(pwd)/.acme.sh
 
 echo "$GREEN 5. Check email and cloudflare API key $NORMAL"
 if [[ -z "$CF_Email"]]; then
-    echo "$RED Please add CF_Email to the environment variables $NORMAL"
+    echo -e "$RED Please add CF_Email to the environment variables $NORMAL"
     exit 1
 fi
 if [[ -z "$CF_Key"]]; then
-    echo "$RED Please add CF_Key to the environment variables $NORMAL"
+    echo -e "$RED Please add CF_Key to the environment variables $NORMAL"
     exit 1
 fi
 
-echo "$GREEN 6. Register account $NORMAL"
+echo -e "$GREEN 6. Register account $NORMAL"
 acme.sh --register-account -m $CF_Email
 
-echo "$GREEN 7. Issue certificate $NORMAL"
+echo -e "$GREEN 7. Issue certificate $NORMAL"
 echo -n "$BLUE Domian: $NORMAL"
 read domain
 acme.sh --issue --dns dns_cf -d $domain
